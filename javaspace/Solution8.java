ad347b898e362d2169922302bf06b6080086f9aa
정상화 <jungsh930210@gmail.com>
2022-07-20 03:34:05 +0900

    edit

--- javaspace/Solution8.java
+++ javaspace/Solution8.java
@@ -3,24 +3,13 @@ import java.io.*;
 
 // Codility
 
-public class Solution7 {
+public class Solution8 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-
-        Integer N = Integer.parseInt(br.readLine());
-
-
-
-        for(int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            System.out.println(st);
-
-
-        }
         
-
-        br.close();
+        System.out.println("Hello Java");
+        
+        System.out.println("Hello Java");
     }
+    
     
 }
