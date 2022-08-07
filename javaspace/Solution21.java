108d755a6b798d7ce47046efdde26df7b04c1f43
정상화 <jungsh930210@gmail.com>
2022-08-07 17:31:22 +0900

    edit

---
+++ javaspace/Solution21.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// Codility - Dominator
+public class Solution21 {
+    public static void main(String[] args) {
+        int[] A = {23171,21011,21123,21366,21013,21367};
+        
+        System.out.println(solution(A));
+    }
+
+    public static int solution(int[] A) {
+        // write your code in Java SE 11
+        int answer = 0;
+        
+        // int len = A.length;
+        
+        // if (len < 2) {
+        //     return 0;
+        // }
+        
+        
+        // int min = A[0];
+        
+        // for (int i=0; i<len; i++) {
+        //     if (A[i] < min) {
+        //         min = A[i];
+        //     } else {
+        //         answer = Math.max(A[i] - min, answer);
+        //     }
+        // }
+
+        return answer;
+    }
+}
