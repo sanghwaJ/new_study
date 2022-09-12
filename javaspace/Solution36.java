677c10846e561743b554587548942e4b94d65ea5
정상화 <jungsh930210@gmail.com>
2022-09-13 01:45:11 +0900

    edit

--- javaspace/Solution36.java
+++ javaspace/Solution36.java
@@ -1,9 +1,5 @@
 // sTest1
 import java.util.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Solution36 {
     public static void main(String[] args) {
@@ -21,14 +17,82 @@ public class Solution36 {
     public static String[][][] map;
 
     public static int solution1(String[][] map3d) {
-        height = map3d.length;
-        length = map3d[0].length;
-        width = map3d[0][0].length();
-        
-        visit = new boolean[height][length][width];
-        
-        return bfs(0, 0, 0, 0, map3d);
+        q = new LinkedList<Dot>();
+
+        int answer = -1;
+
+        int H = map3d.length;
+        int N = map3d[0].length;
+        int M = map3d[0][0].length();
+
+        map = new String[H][N][M];
+
+        for (int h=0; h<H; h++) {
+            for (int n=0; n<N; n++) {
+                for (int m=0; m<M; m++) {
+                    String value = map3d[h][n].charAt(m)+"";
+                    if ("S".equals(value)) {
+                        q.add(new Dot(h, n, m, 0));
+                    }
+                    if ("O".equals(value)) {
+                        map[h][n][m] = "O";
+                    }
+                    if ("E".equals(value)) {
+                        map[h][n][m] = "E";
+                    }
+                }
+            }
+        }
+
+        while(!q.isEmpty()) {
+            int qLen = q.size();
+
+            for (int i=0; i<qLen; i++) {
+                Dot dot = q.poll();
+                
+                int h = dot.h;
+                int y = dot.y;
+                int x = dot.x;
+                int d = dot.d;
+
+                for (int j=0; j<6; j++) {
+                    int hh = h + hpos[j];
+                    int yy = y + ypos[j];
+                    int xx = x + xpos[j];
+                    int dd = d + 1;
+                    
+                    // 범위를 넘어가면 continue
+                    if (hh < 0 || hh >= H || yy < 0 || yy >= N || xx < 0 || xx >= M) {
+                        continue;
+                    }
+                    System.out.println(Arrays.deepToString(map));
+                    if (map[hh][yy][xx] != null) {
+                        if ("O".equals(map[hh][yy][xx])) {
+                            
+                            map[hh][yy][xx] = "X";
+                            q.add(new Dot(hh, yy, xx, dd));
+                        } else if ("E".equals(map[hh][yy][xx])){
+                            return dd;
+                        }
+                    } 
+                }
+            }
+        }
+        return answer;
     }
+
+    public static class Dot {
+        int h, y, x, d;
+        
+        public Dot(int h, int y, int x, int d) {
+            this.x = h;
+            this.y = y;
+            this.h = x;
+            this.d = d;
+        }
+    }
+    
+    /* solution 1 end */
 
     /* solution 2 start */
     public static boolean[][][] visit;
