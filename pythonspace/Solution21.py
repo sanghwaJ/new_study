8a22b74b86e3635e96b3628d2d5f39a6f9eefc38
SanghwaJ <jungsh930210@gmail.com>
2022-10-14 17:44:19 +0900

    sol22

---
+++ pythonspace/Solution21.py
@@ -0,0 +1,22 @@
+# 프로그래머스 - 게임 맵 최단거리
+
+def solution(maps):
+    answer = 0
+    return answer
+
+def bfs(x, y, maps):
+    
+    
+    
+    pass
+
+class Node:
+    def __init__(self, x, y, cost):
+        self.x = x
+        self.y = y
+        self.cost = cost
+
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+# maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+
+print(solution(maps))
