192a30e82079665f3d4fe80335ce9211d8b1baed
정상화 <jungsh930210@gmail.com>
2022-10-15 13:45:54 +0900

    sol22

--- pythonspace/Solution21.py
+++ pythonspace/Solution21.py
@@ -1,32 +1,47 @@
-# 프로그래머스 - 게임 맵 최단거리
+# 프로그래머스 - 게임 맵 최단거리 => 개발중
 
 from collections import deque
 
-
 def solution(maps):
+    global xLen
     xLen = len(maps)
+    global yLen
     yLen = len(maps[0])
+    global dx
+    dx = [0,1,0,-1]
+    global dy
+    dy = [1,0,-1,0]
     
     visited = [[False] * (yLen) for i in range(xLen)]
     
-    print(maps)
-    print(visited)
-    
-    bfs(0, 0, maps)
-    
-    answer = 0
-    return answer
+    bfs(0, 0, maps, visited)
 
-# 최단경로 탐색 문제 => DFS 사용
-def bfs(x, y, maps):
+    return bfs(0, 0, maps, visited)
+
+# 최단경로 탐색 문제 => BFS 사용
+def bfs(x, y, maps, visited):
     queue = deque()
-    node = Node(x, y, 1)
-    # queue.appendleft(node)
+    queue.appendleft(Node(x, y, 1))
+    visited[x][y] = True
     
-    # for i in range(len(queue)):
-    #     print(int(queue[i].x) + " " + int(queue[i].y) + " " + int(queue[i].cost))
-    
-    pass
+    while queue:
+        node = queue.popleft()
+        
+        if node.x == xLen-1 and node.y == yLen-1:
+            return node.cost
+        
+        for i in range(4):
+            xx = node.x + dx[i]
+            yy = node.y + dy[i]
+            
+            if xx < 0 or xx > xLen-1 or yy < 0 or yy > yLen-1:
+                continue
+            
+            if visited[xx][yy] == False and maps[xx][yy] == 1:
+                visited[xx][yy] = True
+                queue.appendleft(Node(xx, yy, node.cost + 1))
+                
+        return -1
 
 class Node:
     def __init__(self, x, y, cost):
