8ea69280d7ba3da50073558048f6a27befb1deef
정상화 <jungsh930210@gmail.com>
2022-10-31 01:54:38 +0900

    edit

--- pythonspace/Solution23.py
+++ pythonspace/Solution23.py
@@ -1,9 +1,6 @@
 # 프로그래머스 - 아이템 줍기
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    # 2차원 리스트 정렬???
-    #rectangle = sorted(rectangle, key=lambda x : (-x[0], -x[1]))
-    
-    #print (rectangle)
+    rectangle = sorted(rectangle, key=lambda x : (x[0], x[1]))
     
     dotList = []
     for i in range(len(rectangle)):
@@ -12,10 +9,16 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
         dot3 = [rectangle[i][2], rectangle[i][1]]
         dot4 = [rectangle[i][2], rectangle[i][3]]
         
-        dotList.append(dot1)
-        dotList.append(dot2)
-        dotList.append(dot3)
-        dotList.append(dot4)
+        if len(dotList) != 0:
+            dotCheck(dotList, dot1)
+            dotCheck(dotList, dot2)
+            dotCheck(dotList, dot3)
+            dotCheck(dotList, dot4)
+        else:
+            dotList.append(dot1)
+            dotList.append(dot2)
+            dotList.append(dot3)
+            dotList.append(dot4)
         
     print(dotList)
         
@@ -24,7 +27,10 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
     return answer
 
-
+def dotCheck(dotList, dot):
+    for i in range(len(dotList)):
+        pass
+    
 # 정답
 rectangle = [[[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]],
              [[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]],
