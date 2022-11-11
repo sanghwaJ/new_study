727f37e7e296a22b5b533237d07cb83f92ae204c
SanghwaJ <jungsh930210@gmail.com>
2022-11-11 17:28:23 +0900

    edit

---
+++ javaspace/Solution51.java
@@ -0,0 +1,32 @@
+// 프로그래머스 - 택배상자
+import java.util.*;
+
+public class Solution51 {
+    public static void main(String[] args) {
+        int[] cacheSize = {3, 3, 2, 5, 2, 0};
+        String[][] cities = {
+                            {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"},
+                            {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"},
+                            {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"},
+                            {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"},
+                            {"Jeju", "Pangyo", "NewYork", "newyork"},
+                            {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"}
+                        };
+        int[] answerList = {50, 21, 60, 52, 16, 25};
+        
+        
+        for (int i=0; i<cacheSize.length; i++) {
+            int answer = solution(cacheSize[i], cities[i]);
+            if (answerList[i] == answer) {
+                System.out.println("정답 => " + answer);   
+            } else {
+                System.out.println("오답 => " + answer); 
+            }
+        }
+    }
+
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        return answer;
+    }
+}
