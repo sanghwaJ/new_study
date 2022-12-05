c8ca7d87825ff24a2207076e1f77d9468a85b23e
SanghwaJ <jungsh930210@gmail.com>
2022-12-05 17:38:36 +0900

    sol55

--- javaspace/Solution55.java
+++ javaspace/Solution55.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class Solution55 {
     public static void main(String[] args) {
-        int N = 1030000230;
+        int A = 6;
+        int B = 11;
+        int K = 2;
         
-        System.out.println(solution(N));
+        System.out.println(solution(A, B, K));
     }
 
     public static int solution(int A, int B, int K) {
